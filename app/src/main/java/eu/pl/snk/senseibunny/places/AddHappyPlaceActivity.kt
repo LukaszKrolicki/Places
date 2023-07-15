37396edea979e19929722de076bcb41bcf3caf16
SesneiBunny <lukasmatrix12345@gmail.com>
2023-07-15 16:03:39 +0200

    add page layout constructed

--- app/src/main/java/eu/pl/snk/senseibunny/places/AddHappyPlaceActivity.kt
+++ app/src/main/java/eu/pl/snk/senseibunny/places/AddHappyPlaceActivity.kt
@@ -1,11 +1,61 @@
 package eu.pl.snk.senseibunny.places
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import eu.pl.snk.senseibunny.places.databinding.ActivityAddHappyPlaceBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddHappyPlaceActivity : AppCompatActivity() {
+
+    private var binding: ActivityAddHappyPlaceBinding ?=null
+
+    private var tvSelectedDate: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_happy_place)
+        binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolBar)
+
+        if(supportActionBar!=null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
+        binding?.toolBar?.setNavigationOnClickListener{
+            onBackPressed()
+        }
+
+        binding?.date?.setOnClickListener{
+            clickDatePicker()
+        }
+
+
+    }
+
+    private fun clickDatePicker() {
+
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        val dpd = DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDayOfMonth ->
+
+                val selectedDate = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
+                Toast.makeText(this, selectedDate.toString(), Toast.LENGTH_LONG).show()
+                binding?.date?.setText(selectedDate)
+            },
+            year,
+            month,
+            day
+        )
+        dpd.datePicker.maxDate= System.currentTimeMillis()-86400000 // setting max date to yesterday
+        dpd.show()
     }
 }
--- app/src/main/java/eu/pl/snk/senseibunny/places/MainActivity.kt
+++ app/src/main/java/eu/pl/snk/senseibunny/places/MainActivity.kt
@@ -1,11 +1,27 @@
 package eu.pl.snk.senseibunny.places
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import eu.pl.snk.senseibunny.places.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root) // top element of xml file is root
+
+        binding?.fabAddHappyPlace?.setOnClickListener{
+            val intent = Intent(this,AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding=null
     }
 }

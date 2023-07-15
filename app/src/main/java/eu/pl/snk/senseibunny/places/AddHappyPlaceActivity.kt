ca166d442a27dc4eacee7781cc02ca8603c677e5
SesneiBunny <lukasmatrix12345@gmail.com>
2023-07-15 14:21:39 +0200

    setting up, floating button added

---
+++ app/src/main/java/eu/pl/snk/senseibunny/places/AddHappyPlaceActivity.kt
@@ -0,0 +1,11 @@
+package eu.pl.snk.senseibunny.places
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddHappyPlaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_happy_place)
+    }
+}

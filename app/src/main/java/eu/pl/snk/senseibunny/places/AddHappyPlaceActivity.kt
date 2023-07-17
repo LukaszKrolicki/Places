50d882e7b76b2583752b6ef58f7cdf02a074deff
SesneiBunny <lukasmatrix12345@gmail.com>
2023-07-18 00:31:19 +0200

    permissions added

--- app/src/main/java/eu/pl/snk/senseibunny/places/AddHappyPlaceActivity.kt
+++ app/src/main/java/eu/pl/snk/senseibunny/places/AddHappyPlaceActivity.kt
@@ -1,12 +1,23 @@
 package eu.pl.snk.senseibunny.places
 
+import android.app.AlertDialog
 import android.app.DatePickerDialog
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import eu.pl.snk.senseibunny.places.databinding.ActivityAddHappyPlaceBinding
-import java.text.SimpleDateFormat
 import java.util.*
 
 class AddHappyPlaceActivity : AppCompatActivity() {
@@ -14,6 +25,10 @@ class AddHappyPlaceActivity : AppCompatActivity() {
     private var binding: ActivityAddHappyPlaceBinding ?=null
 
     private var tvSelectedDate: TextView? = null
+
+    private var buttonAnim: Button ?=null
+
+    private var animation: Animation?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
@@ -33,6 +48,59 @@ class AddHappyPlaceActivity : AppCompatActivity() {
             clickDatePicker()
         }
 
+        buttonAnim=binding?.breathButton
+
+        animation= AnimationUtils.loadAnimation(this,R.anim.breath)
+        buttonAnim?.startAnimation(animation)
+
+        binding?.uploadImage?.setOnClickListener{
+            val pictureDialog = AlertDialog.Builder(this)
+            pictureDialog.setTitle("Select Action")
+            val pictureDialogItems = arrayOf("Choose from Gallery", "Take a Picture")
+            pictureDialog.setItems(pictureDialogItems) { dialog, which ->
+                when(which){
+                    0->chooseFromGallery()
+                    1-> Toast.makeText(this, "Camera selection coming soon", Toast.LENGTH_SHORT).show()
+                }
+            }
+            pictureDialog.show()
+        }
+
+
+    }
+
+    private fun chooseFromGallery(){
+        Dexter.withContext(this).withPermissions(
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ).withListener(object: MultiplePermissionsListener {
+
+                override fun onPermissionsChecked(report: MultiplePermissionsReport) {if(report.areAllPermissionsGranted()){
+                    Toast.makeText(this@AddHappyPlaceActivity, "Permission Granted", Toast.LENGTH_SHORT).show()
+                }
+
+                }
+                override fun onPermissionRationaleShouldBeShown(permissions: MutableList<PermissionRequest>, token: PermissionToken) {
+                    showRationaleDialogForPermissions()
+                    token?.cancelPermissionRequest()
+                }
+            }).onSameThread().check();
+
+    }
+
+    private fun showRationaleDialogForPermissions(){
+        AlertDialog.Builder(this).setMessage("It looks like you didnt give permissions thats sad L").setPositiveButton("Go to Settings") { dialog, which ->
+            try{
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            } catch (e:Exception){
+                e.printStackTrace()
+            }
+        }.setNegativeButton("Cancel") { dialog, which ->
+            dialog.dismiss()
+        }.show()
 
     }
 
@@ -57,5 +125,7 @@ class AddHappyPlaceActivity : AppCompatActivity() {
         )
         dpd.datePicker.maxDate= System.currentTimeMillis()-86400000 // setting max date to yesterday
         dpd.show()
+
     }
+
 }
